b8e323fb09db9b677880478aa06aa6633bda3df6
Sean Carlisle <scarlisle@hylamobile.com>
2018-11-16 11:26:44 -0600

    asdf

--- MavBot.java
+++ MavBot.java
@@ -14,6 +14,8 @@ import java.io.FileOutputStream;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Random;
 
 public class MavBot {
@@ -91,20 +93,43 @@ public class MavBot {
     private static void outputBoard(MapCell[][] map) {
         Long tot = 0l;
         int max = 0;
+        int min = 64000;
+        List<Integer> cells = new ArrayList<>();
         StringBuilder rows = new StringBuilder();
         for (MapCell[] row : map) {
             for (MapCell cell : row) {
+                cells.add(cell.halite);
                 rows.append(cell.halite).append("\t");
-                tot+=cell.halite;
-                if (cell.halite>max)
+                tot += cell.halite;
+                if (cell.halite > max)
                     max = cell.halite;
+                if (cell.halite < min)
+                    min = cell.halite;
             }
             rows.append("\n");
         }
+        cells.sort(Comparator.naturalOrder());
+        int range = max - min;
+        int qSize = range / 4;
+        int q1 = 0, q2 = 0, q3 = 0, q4 = 0;
+        for (Integer c : cells) {
+            if (c < qSize) {
+                q1++;
+            } else if (c < qSize * 2) {
+                q2++;
+            } else if (c < qSize * 3) {
+                q3++;
+            } else {
+                q4++;
+            }
+        }
+
         Log.log(rows.toString());
         Log.log("Total: " + tot);
         Log.log("MAX: " + max);
         Log.log("AVG: " + tot / (map.length * map[0].length));
+        Log.log("QUANTILE SIZE:\t<"+qSize+"\t<"+qSize*2+"\t<"+qSize*3+"\t<"+qSize*4);
+        Log.log("Distribution: \tQ1:" + q1 + "\tQ2:" + q2 + "\tQ3:" + q3 + "\tQ4:" + q4);
 
     }
 }
